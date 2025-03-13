4304b93dfe905aee03774de69e149ca02ab51268
tehaulp <fauatehau02@gmail.com>
2025-03-12 16:00:46 -1000

    improved available games list

--- src/components/sections/join/GameList.tsx
+++ src/components/sections/join/GameList.tsx
@@ -12,17 +12,23 @@ export default function GameList({
   playerUsername,
 }: GameListProps) {
   return (
-    <div>
-      <h2>Parties disponibles</h2>
-      <ul>
+    <div className="w-full max-w-xl">
+      <h2 className="text-3xl font-bold text-center mb-4">Parties disponibles</h2>
+      <ul className="space-y-4 flex flex-col gap-y-2 max-h-60 overflow-y-scroll custom-scrollbar">
         {games.map((game) => (
-          <li key={game.game_id}>
-            <div>
-              <p>{game.game_name}</p>
-              <p>Hôte: {game.host_username}</p>
-              <p>{game.game_status}</p>
+          <li
+            key={game.game_id}
+            className="flex justify-between items-center p-1"
+          >
+            <div className="flex flex-row space-x-2 items-center">
+              <p className="text-xl font-semibold">{game.game_name}</p>
+              <p>[{game.host_username}]</p>
             </div>
-            <button onClick={() => joinGame(game.game_id, playerUsername)}>
+
+            <button
+              onClick={() => joinGame(game.game_id, playerUsername)}
+              className="border hover:bg-blue-500 p-2"
+            >
               Rejoindre
             </button>
           </li>
